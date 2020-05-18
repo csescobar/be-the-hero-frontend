9ae50feeabc26f118eaee0fa9528d7bbfaee875a
Cesar Escobar <cesarsilvaescobar@gmail.com>
2020-05-18 13:47:47 -0300

    Finalização da aula

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,20 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import {FiLogIn} from 'react-icons/fi';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
+
 import './styles.css';
 
-import logoImg from '../../assets/logo.svg' 
+import logoImg from '../../assets/logo.svg'
 import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
+  const [id, setId] = useState();
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      console.log(id);
+      const response = await api.post('sessions', {id});
+
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+      
+      history.push('/profile');
+    } catch (error) {
+      alert(`Falha no Login ${error}`);
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
-        <img src={logoImg} alt="Be The Hero"/>
-        <form action="">
+        <img src={logoImg} alt="Be The Hero" />
+        <form onSubmit={handleLogin} >
           <h1>Faça seu Logon</h1>
-          <input placeholder="Sua ID"/>
-          <button className="button" type="submit" >Entrar</button>
+          <input 
+            placeholder="Sua ID" 
+            onChange={e => setId(e.target.value)} 
+          />
+          <button className="button" type="submit">Entrar</button>
 
           <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#E02041" />
@@ -22,7 +47,7 @@ export default function Logon() {
           </Link>
         </form>
       </section>
-      <img src={heroesImg} alt="Heroes"/>
+      <img src={heroesImg} alt="Heroes" />
     </div>
-    );
+  );
 };
